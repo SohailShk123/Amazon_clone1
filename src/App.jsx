2057fd4f3df5b3f253a9c5921ef69b0069840904
Sohail <sohailshaikh2636@gmail.com>
2023-08-18 18:03:39 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect } from 'react'
-import './App.css'
+import React from 'react'
 import { Routes,Route } from "react-router-dom";
 import Home from './Home';
 import SignUp from './pages/SignUp';
